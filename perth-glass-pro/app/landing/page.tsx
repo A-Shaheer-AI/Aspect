c67266c38fc225ff4d1ea3a3bfe884e14d31447b
zaidkhanvmi <zaidkhan.vmi@gmail.com>
2026-03-26 23:57:43 -0700

    changed the before-after-image

--- perth-glass-pro/app/landing/page.tsx
+++ perth-glass-pro/app/landing/page.tsx
@@ -313,8 +313,8 @@ export default function LandingPage() {
                         />
 
                         <BeforeAfterSlider
-                            afterImage="https://res.cloudinary.com/dr8tjrszy/image/upload/v1772792155/aspect-before-window-cleaning_zfr8ae.jpg"
-                            beforeImage="https://res.cloudinary.com/dr8tjrszy/image/upload/v1772792157/after-window-cleaning_fs1hhz.jpg"
+                            afterImage="https://res.cloudinary.com/dr8tjrszy/image/upload/v1772792157/after-window-cleaning_fs1hhz.jpg"
+                            beforeImage="https://res.cloudinary.com/dr8tjrszy/image/upload/v1772792155/aspect-before-window-cleaning_zfr8ae.jpg"
                             initial={50}
                         />
 
